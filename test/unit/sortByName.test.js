16e11a90fcabcaf1c8c78236a93f40a9874a9008
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-11 13:17:07 +0400

    added new test

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -14,4 +14,22 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  it("Books names should remain sorted in ascending order if already sorted", () => {
+    const input = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер"
+    ];
+
+    const expected =  [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер"
+    ];
+  
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+  
 });
