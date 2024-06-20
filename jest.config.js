e9c3cc9da152e9a25b771f1801e44b3e868d03a1
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-20 12:48:33 +0400

    Changed coverageThreshold to 100

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ const config = {
 
     coverageThreshold: {
         global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100
         }
       }
   };
